33d191e5a9ea58628ba311a34aecd4f233bef8ce
svasandani <vasandanishailesh@gmail.com>
2020-07-23 16:37:38 -0400

    Add dynamic group management to Group.js

--- src/components/groups/Groups.js
+++ src/components/groups/Groups.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  // Link,
+  Link,
   useRouteMatch,
   useParams
 } from 'react-router-dom';
@@ -13,11 +13,15 @@ import UserGroups from '../landing/UserGroups';
 function Groups() {
   let match = useRouteMatch();
 
+  const [data, setData] = useState({ id: 0, lings: [] });
+  const [lingData, setLingData] = useState({ id: 0, lings: [] });
+
   return (
     <Router>
       <Switch>
         <Route path={`${match.path}/:groupId`}>
-          <Group />
+          <Group data={data} setData={setData} lingData={lingData} setLingData={setLingData} />
+          <Link to={`${match.path}/8`}>hello</Link>
         </Route>
         <Route path={match.path}>
           <UserGroups />
@@ -27,9 +31,20 @@ function Groups() {
   )
 }
 
-function Group() {
+function Group({ data, setData, lingData, setLingData }) {
   let { groupId } = useParams();
-  return <h3>Requested group ID: {groupId}</h3>;
+
+  useEffect(() => {
+    fetch("https://ui.terraling.com/api/group/" + groupId)
+      .then(response => response.json())
+      .then(data => { setData(data) });
+
+    fetch("https://ui.terraling.com/api/lings/" + groupId)
+      .then(response => response.json())
+      .then(lingData => { setLingData(lingData) });
+  }, [groupId]);
+
+  return <h3>Requested group ID: {JSON.stringify(lingData)}</h3>;
 }
 
 export default Groups;
