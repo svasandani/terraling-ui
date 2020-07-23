11a06ad53f48da72f45536000442e8ca3a51a6f9
svasandani <vasandanishailesh@gmail.com>
2020-07-23 15:04:24 -0400

    Render user groups when no group id chosen

--- src/components/groups/Groups.js
+++ src/components/groups/Groups.js
@@ -3,28 +3,27 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
+  // Link,
   useRouteMatch,
   useParams
 } from 'react-router-dom';
+
+import UserGroups from '../landing/UserGroups';
 
 function Groups() {
   let match = useRouteMatch();
 
   return (
-    <>
-      <Link to={`${match.url}/components`}>Components</Link>
-      <h1>group</h1>
-
+    <Router>
       <Switch>
         <Route path={`${match.path}/:groupId`}>
           <Group />
         </Route>
         <Route path={match.path}>
-          <h3>Please select a topic.</h3>
+          <UserGroups />
         </Route>
       </Switch>
-    </>
+    </Router>
   )
 }
 
