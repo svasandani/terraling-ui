d2b1f3963a1615ed241259c0008aa9e4d27f613d
svasandani <vasandanishailesh@gmail.com>
2020-07-24 19:05:57 -0400

    Add filtering to GroupLings

--- src/components/groups/GroupLings.js
+++ src/components/groups/GroupLings.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import '../../css/groups/GroupLings.css';
 
 import { CapitalCase, TargetToPlural } from '../helpers/Helpers';
 
 function GroupLings({ lingData, lingName }) {
+  const [filter, setFilter] = useState("");
 
   let lingReduce = {};
   lingData.reduce((acc, ling) => {
@@ -18,22 +21,35 @@ function GroupLings({ lingData, lingName }) {
     }
   }, {});
 
-  console.log(lingReduce);
+  const filterBy = (e, letter) => {
+    e.preventDefault();
+    setFilter(letter);
+  }
 
   return (
     <main>
       <section id="container">
         <h1>{CapitalCase(TargetToPlural(2, lingName))}</h1>
+        <div className="card filter">
+          <a href="#container" onClick={(e) => { filterBy(e, "") }}>ALL</a>
+          { Object.keys(lingReduce).sort().map(letter => {
+            return (
+              <a key={letter} href="#container" onClick={(e) => { filterBy(e, letter) }}>{letter}</a>
+            )
+          })}
+        </div>
         { Object.keys(lingReduce).sort().map(letter => {
+          if (filter.length > 0 && filter !== letter) return null;
+
           return (
             <div key={letter} className="letter">
               <h2>{letter}</h2>
               <div className="card">
                 { lingReduce[letter].map((ling, i) => {
                   return (
-                    <Link key={i} to={ling.id}>
-                      <div className="ling">
-                        <a><span className="name">{ling.name}</span></a>
+                    <React.Fragment key={i}>
+                      <div key={i} className="ling">
+                        <Link to={`${ling.id}`}><span className="name">{ling.name}</span></Link>
                       </div>
                       { lingReduce[letter].length - i > 1 ?
                         (
@@ -43,7 +59,7 @@ function GroupLings({ lingData, lingName }) {
                           null
                         )
                       }
-                    </Link>
+                    </React.Fragment>
                   )
                 }) }
               </div>
