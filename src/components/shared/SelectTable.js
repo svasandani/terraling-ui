2fabb6639ce0aa831a553444566d4c57eeca1e6e
svasandani <vasandanishailesh@gmail.com>
2020-07-25 19:03:36 -0400

    Add custom find to SelectTable

--- src/components/shared/SelectTable.js
+++ src/components/shared/SelectTable.js
@@ -9,11 +9,16 @@ import '../../css/shared/SelectTable.css';
   data - Set of data to be displayed as a table, must be an Array
   sort - Callback that allows for sorting of values, must take in two args
          and return 1 if the first is greater, and -1 otherwise
+  find - Callback that allows for custom find methods (defaults to checking name prop)
   selectArr - Array that contains all selected objects in table (extracted from data)
+  setSelectArr - State setter for selectArr
+  maxSelect - Max number of selections to permit
   columnMap - Array representing the columns in the table, along with their keys in data; the
               first element in the array (i.e. first column) is what will be alphabetically separated.
+  maxHeight - Max height in px of card
+  disabled - Whether to gray out card
 */
-function SelectTable({ data, sort, selectArr, setSelectArr, columnMap }) {
+function SelectTable({ data, sort, find, selectArr, setSelectArr, maxSelect=6, columnMap, maxHeight, disabled }) {
   if (data.length === 0) return (
     <Loading />
   );
@@ -22,23 +27,29 @@ function SelectTable({ data, sort, selectArr, setSelectArr, columnMap }) {
     data = data.sort(sort);
   }
 
+  if (find === undefined) {
+    find = (el, row) => el.name === row.name
+  }
+
   const toggle = (index, row) => {
+    if (disabled) return;
+
     let newSelectArr = [...selectArr];
 
     if (index >= 0) newSelectArr.splice(index, 1);
-    else newSelectArr.push(row);
-
-    console.log(selectArr === newSelectArr);
+    else if (selectArr.length < maxSelect) newSelectArr.push(row);
+    else {
+      newSelectArr.shift();
+      newSelectArr.push(row);
+    }
 
     setSelectArr(newSelectArr);
   };
 
   return (
-    <div className="card">
-      { data.sort().map((row, i) => {
-        let contains = false;
-
-        let index = selectArr.indexOf(row);
+    <div className={`card ${disabled ? "disabled" : ""}`} style={{ "maxHeight": maxHeight ? maxHeight : "auto", "overflowY": maxHeight ? "scroll" : "visible" }} >
+      { data.map((row, i) => {
+        let index = selectArr.findIndex((el) => { return find(el, row); });
 
         return (
           <React.Fragment key={i}>
