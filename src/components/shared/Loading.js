83d9a26343b4ec2133354e825790877ad88e66ff
svasandani <vasandanishailesh@gmail.com>
2020-07-25 02:37:34 -0400

    Revise Loading screen to be more appealing

--- src/components/shared/Loading.js
+++ src/components/shared/Loading.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import '../../css/shared/Loading.css';
+import loading from '../../img/loading.svg';
+
 function Loading() {
   return (
-    <main>
-      <section id="loading">
-        <h1>Loading... please wait.</h1>
-      </section>
-    </main>
+    <div className="loading">
+      <h1 className="ellipsis">Hold on</h1>
+      <img src={loading} alt="A person turning several gears." />
+      <h2>We're crunching the numbers.</h2>
+    </div>
   )
 }
 
