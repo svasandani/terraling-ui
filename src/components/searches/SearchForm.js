241319fe9a79977c816966d7d622aee3193abc62
svasandani <vasandanishailesh@gmail.com>
2020-07-25 19:03:19 -0400

    Add search type and language cards to SearchForm

--- src/components/searches/SearchForm.js
+++ src/components/searches/SearchForm.js
@@ -2,15 +2,21 @@ import React, { useState, useEffect } from 'react';
 
 import SelectTable from '../shared/SelectTable';
 
+import { CapitalCase, TargetToPlural } from '../helpers/Helpers';
+
 function SearchForm({ data }) {
   const [selectArr, setSelectArr] = useState([]);
+  const [searchTypesArr, setSearchTypesArr] = useState([]);
 
-  useEffect(() => {
-    console.log(selectArr);
-  }, [selectArr]);
+  const searchTypes = [{"name": "Any"}, {"name": "Compare"}, {"name": "Cross"}]
 
   return (
-    <SelectTable data={data.lingData} columnMap={["name"]} selectArr={selectArr} setSelectArr={setSelectArr} />
+    <>
+      <h2>Search type</h2>
+      <SelectTable data={searchTypes} columnMap={["name"]} selectArr={searchTypesArr} setSelectArr={setSearchTypesArr} maxSelect={1} />
+      <h2>{CapitalCase(TargetToPlural(2, data.overviewData.ling0_name))}</h2>
+      <SelectTable data={data.lingData} columnMap={["name"]} selectArr={selectArr} setSelectArr={setSelectArr} maxHeight="250px" />
+    </>
   )
 }
 
