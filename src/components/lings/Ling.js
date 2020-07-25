7426ee7de9e59976d9af0e70978c7debd441c290
svasandani <vasandanishailesh@gmail.com>
2020-07-25 18:16:04 -0400

    Create common Table stylesheet

--- src/components/lings/Ling.js
+++ src/components/lings/Ling.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {
-  useRouteMatch,
-  useParams
-} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import AlphaTable from '../shared/AlphaTable';
 
