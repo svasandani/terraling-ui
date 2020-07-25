3ed5f2f7c2fed30097601ec859ca8292a6976d9d
svasandani <vasandanishailesh@gmail.com>
2020-07-25 03:11:13 -0400

    Revise mobile header flyout

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,24 +20,25 @@ function Header({ signedin, setSignedIn, user }) {
         <span className="logo"><Link to="/">Terraling</Link></span>
         <input type="checkbox" id="flyout-input" />
         <label htmlFor="flyout-input" className="flyout"><img src="https://img.icons8.com/fluent/48/000000/menu--v2.svg" alt="Flyout menu" /></label>
-        <div className="sandwich">
-          <a href="explore">Explore</a>
-          <a href="project_groups">Project Groups</a>
-          <a href="help">Help</a>
+        <div className="flyout-target">
+          <div className="sandwich">
+            <a href="explore">Explore</a>
+            <a href="project_groups">Project Groups</a>
+            <a href="help">Help</a>
+          </div>
+          {signedin ?
+          (
+            <div className="user-nav">
+              <a href="users">Shailesh Vasandani</a>
+              <a className="button" href="." onClick={signOut}>Sign out</a>
+            </div>
+          ) :
+          (
+            <div className="user-nav">
+              <a className="button" href="." onClick={signIn}>Sign in</a>
+            </div>
+          )}
         </div>
-        {signedin ?
-        (
-          <div className="user-nav">
-            <a href="users">Shailesh Vasandani</a>
-            <a className="button" href="." onClick={signOut}>Sign out</a>
-          </div>
-        ) :
-        (
-          <div className="user-nav">
-            <a className="button" href="." onClick={signIn}>Sign in</a>
-          </div>
-        )}
-
       </nav>
     </header>
   )
