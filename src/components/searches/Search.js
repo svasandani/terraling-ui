f0edaa0c713f7f6f4a07ecc62d53a8cbb4b5e480
svasandani <vasandanishailesh@gmail.com>
2020-07-25 18:17:03 -0400

    Rename Searches to Search

--- src/components/searches/Search.js
+++ src/components/searches/Search.js
@@ -2,26 +2,30 @@ import React, { useState } from 'react';
 import {
   Switch,
   Route,
-  useRouteMatch,
-  useParams
+  useRouteMatch
 } from 'react-router-dom';
 
 import SearchForm from './SearchForm';
 import SearchResults from './SearchResults';
 
-function Searches() {
+function Search({ data }) {
   let match = useRouteMatch();
-  
+
+  console.log(match.path);
+
   return (
     <Switch>
-      <Route path={`${match.path}/preview`}>
+      <Route path={`${match.path}/new`}>
+        <SearchForm data={data} />
+      </Route>
+      <Route path={`${match.path}/results`}>
         <SearchResults />
       </Route>
-      <Route path={match.path}>
-        <SearchForm />
+      <Route path={`${match.path}`}>
+        <h1>Hi</h1>
       </Route>
     </Switch>
   )
 }
 
-export default Searches;
+export default Search;
