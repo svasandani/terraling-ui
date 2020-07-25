72726e36a8c1b484b8276ff3090a66e81e6ab67b
svasandani <vasandanishailesh@gmail.com>
2020-07-25 02:41:16 -0400

    Create Ling component

--- src/components/lings/Ling.js
+++ src/components/lings/Ling.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  useRouteMatch,
+  useParams
+} from 'react-router-dom';
 
-function Ling() {
+import AlphaTable from '../shared/AlphaTable';
 
+import Loading from '../shared/Loading';
+
+function Ling({ groupId }) {
+  let match = useRouteMatch();
+
+  const [ready, setReady] = useState(false);
+
+  const [data, setData] = useState({ ling_name: "", ling_properties: [] })
+
+  const columnMap = ["property_name", "value"];
+
+  let { lingId } = useParams();
+
+  useEffect(() => {
+    fetch("http://192.168.0.19:4000/groups/" + groupId + "/lings/" + lingId, { headers:{'accept': 'application/json'} })
+        .then(response => response.json())
+        .then((data) => {
+          setData(data);
+          setReady(true);
+        });
+  }, [lingId]);
+
+  if (!ready) return(<Loading />)
+
+  console.log(data);
+
+  return (
+    <>
+      <h1>{data.ling_name}</h1>
+      <h2>Description</h2>
+      <h2>Compare</h2>
+      <h2>Properties</h2>
+      <AlphaTable name={data.ling_name} data={data.ling_properties} columnMap={columnMap} />
+    </>
+  )
 }
 
 export default Ling;
