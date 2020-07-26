8c0e424be84ccbf803d35107f0c04a558c06b419
svasandani <vasandanishailesh@gmail.com>
2020-07-26 16:02:31 -0400

    Add random loading messages

--- src/components/shared/Loading.js
+++ src/components/shared/Loading.js
@@ -4,11 +4,17 @@ import '../../css/shared/Loading.css';
 import loading from '../../img/loading.svg';
 
 function Loading() {
+  const headingChoices = ["Hold on", "Please wait", "One moment", "Just a sec"];
+  let headingIndex = Math.floor(Math.random() * 4);
+
+  const textChoices = ["We're crunching the numbers.", "We're fetching your data.", "We're working our magic.", "We'll be ready shortly."];
+  let textIndex = Math.floor(Math.random() * 4);
+
   return (
     <div className="loading">
-      <h1 className="ellipsis">Hold on</h1>
+      <h1 className="ellipsis">{headingChoices[headingIndex]}</h1>
       <img src={loading} alt="A person turning several gears." />
-      <h2>We're crunching the numbers.</h2>
+      <h2>{textChoices[textIndex]}</h2>
     </div>
   )
 }
