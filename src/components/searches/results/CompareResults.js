0a61bcda1e364ab8682c54fc7b0d1fbdbbc4e182
svasandani <vasandanishailesh@gmail.com>
2020-07-27 00:30:23 -0400

    Build results for Compare Search

--- src/components/searches/results/CompareResults.js
+++ src/components/searches/results/CompareResults.js
@@ -9,10 +9,57 @@ function CompareResults({ data, resultData }) {
   let num = lings.length;
   let depth = lings[0].depth;
 
-  lings = lings.reduce((arr, ling) => { arr.push(ling.name); return arr; }, [])
+  lings = lings.reduce((arr, ling) => { arr.push(ling.name); return arr; }, []);
+
+  let diffColumnMap = { name: resultData.header.differents.compare_property };
+  resultData.header.differents.ling_value.forEach((value, i) => {
+    diffColumnMap["value_" + i] = value;
+  });
+
+  let separatedData = resultData.rows.reduce((arr, row) => {
+    if (row.child.length > 1) {
+      let diffObj = { name: row.parent[0].property.name, id: row.parent[0].property.id };
+
+      for ( let i = 0; i < num; i++ ) {
+        diffObj["value_" + i] = row.child[i] === null ? "" : row.child[i].value;
+      }
+
+      arr.differents.push(diffObj);
+    } else {
+      arr.commons.push({ name: row.parent[0].property.name, id: row.parent[0].property.id, value: row.child[0].value });
+    }
+
+    return arr;
+  }, { commons: [], differents: [] })
 
   return (
-    <h2>Comparing {num} {CapitalCase(TargetToPlural(2, (depth > 0 ? data.overviewData.ling1_name : data.overviewData.ling0_name)))}: {lings.join(", ")}</h2>
+    <>
+      <h1>Comparing {num} {CapitalCase(TargetToPlural(2, (depth > 0 ? data.overviewData.ling1_name : data.overviewData.ling0_name)))}: {lings.join(", ")}</h1>
+      {
+        Object.keys(resultData.header.commons).length > 0 ?
+        (
+          <>
+            <h2>Common Property Values</h2>
+            <HeadingTable data={separatedData.commons} columnMap={{ "name": resultData.header.commons.compare_property, "value": resultData.header.commons.common_values }} link={(url, id) => { return "/groups/" + data.id + "/properties/" + id; }} />
+          </>
+        ) :
+        (
+          null
+        )
+      }
+      {
+        Object.keys(resultData.header.differents).length > 0 ?
+        (
+          <>
+            <h2>Different Property Values</h2>
+            <HeadingTable data={separatedData.differents} columnMap={diffColumnMap} link={(url, id) => { return "/groups/" + data.id + "/properties/" + id; }} />
+          </>
+        ) :
+        (
+          null
+        )
+      }
+    </>
   )
 }
 
