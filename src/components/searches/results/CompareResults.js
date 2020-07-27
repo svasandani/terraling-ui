3c7bead40584eb05d27084beaad53cea7622bd47
svasandani <vasandanishailesh@gmail.com>
2020-07-27 00:28:52 -0400

    Add HeadingTable component for searches

--- src/components/searches/results/CompareResults.js
+++ src/components/searches/results/CompareResults.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import HeadingTable from '../../shared/HeadingTable';
+
 import { CapitalCase, TargetToPlural } from '../../helpers/Helpers';
 
 function CompareResults({ data, resultData }) {
---
+++ src/components/shared/HeadingTable.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+
+import Loading from '../shared/Loading';
+
+import '../../css/shared/Table.css';
+
+function HeadingTable({ data, sort, link, columnMap }) {
+  let keys = Object.keys(columnMap);
+
+  let match = useRouteMatch();
+
+  if (data.length === 0) return (
+    <Loading />
+  );
+
+  if (sort !== undefined) {
+    data = data.sort(sort);
+  }
+
+  if (link === undefined) {
+    link = (url, id) => { return url + "/" + id; }
+  }
+
+  return (
+    <>
+      <div className="card header-card">
+        <div className={`headers row row-${keys.length}`}>
+          {
+            keys.map((col, i) => {
+              return (
+                <span key={i} className="header name">{columnMap[col]}</span>
+              )
+            })
+          }
+        </div>
+        { data.map((row, i) => {
+          return (
+            <React.Fragment key={i}>
+              <div className={`row row-${keys.length}`}>
+                {
+                  keys.map((col, i) => {
+                    if (i === 0) return (
+                      <span key={i} className="name"><Link to={link(match.url, row.id)}>{row[col]}</Link></span>
+                    ); else return (
+                      <span key={i} className="name">{row[col]}</span>
+                    );
+                  })
+                }
+
+              </div>
+              { data.length - i > 1 ?
+                (
+                  <span className="h-divider" />
+                ) :
+                (
+                  null
+                )
+              }
+            </React.Fragment>
+          )
+        }) }
+      </div>
+    </>
+  );
+}
+
+export default HeadingTable;
